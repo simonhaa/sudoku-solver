1b1c1158c584fb86872c3bbb1f4a67e58be3b937
simon <simonh7002@gmail.com>
2026-09-25 02:31:04 -0400

    refactor: enhance checking logic in FSM for Sudoku validation

--- sudoku.v
+++ sudoku.v
@@ -44,7 +44,7 @@ module sudoku
     reg [3:0] latestDigit = 4'd0;           // latest digit used in TRY_DIGIT state
 
     reg [1:0] check_state;
-    reg [3:0] k;                            // 
+    reg [3:0] k;                            // Index that walks across the 9 cells in the row we check
     reg       conflict_found;               // Pulses high once a matching digit is found
     reg       check_start;                  // Main FSM pulses this to start the checking process
     reg       check_done;                   // Sub FSM pulses this once the checking process is completed
@@ -132,29 +132,65 @@ module sudoku
     // Checker sub-FSM block
     always @(posedge clk) begin
         if (btn1) begin
-            check_state <= CHECK_ROW;
+            check_state <= CHECK_ROW; // not sure if this makes sense, check_state, by default (reset), is CHECK_ROW?
             check_done <= 1'b0;
         end else begin
+            // emptyCell is saved when FSM gets to this state --> emptyCell returns the index of the grid
+            // emptyCell = 20 = bottom right of top left box; row = 2; col = 2; box = 0
+            row <= emptyCell / 9; // 2: Row 2
+            col <= emptyCell % 9; // 2: Col 2
+            box <= (row / 3) * 3 + (col / 3); // 0: Box 0
+
             case (check_state)
             CHECK_ROW: begin
                 if (check_start) begin
-                    k <= 4'd0; // k is the specific digit we're checking against
+                    k <= 4'd0;
                     conflict_found <= 1'b0;
-                    check_done <= 1'b0;
-                    check_state <= CHECK_ROW;
+                    check_done <= 1'b0;                 // pulses high after successful CHECK_BOX
                 end
-                // Comparison logic for row:
-                
-                // emptyCell is saved when FSM gets to this state --> emptyCell returns the index of the grid
-                // emptyCell = 20 = bottom right of top left box; row = 2; col = 2; box = 0
-                row <= emptyCell / 9;
-                col <= emptyCell % 9;
-                box <= ; // figure out later
 
-                for ()
-                
+                // Comparison logic for row:
+                if (k != col && (grid[emptyCell] == grid[row*9 + k])) begin
+                    conflict_found <= 1'b1;
+                    check_done <= 1'b1;
+                end else if (k == 8) begin              // successfully iterated to the end of the row without any conflicts
+                    check_state <= CHECK_COL;
+                    check_start <= 1'b1;                // pulse start for next sub-check
+                    k <= 4'd0;
+                end else begin
+                    k <= k + 1'b1;
+                end
             end
                 
+            CHECK_COL: begin
+                if (check_start) begin
+                    k <= 4'd0;
+                    conflict_found <= 1'b0;
+                    check_done <= 1'b0;
+                end
+
+                // Comparison logic for col:
+                if (k != row && (grid[emptyCell] == grid[col + k*9])) begin
+                    conflict_found <= 1'b1;
+                    check_done <= 1'b1;
+                end else if (k == 8) begin
+                    check_state <= CHECK_BOX;
+                    check_start <= 1'b1;                // pulse start for next sub-check
+                end else begin
+                    k <= k + 1'b1;
+                end
+            end
+
+            CHECK_BOX: begin
+                if (check_start) begin
+                    k <= 4'd0;
+                    conflict_found <= 1'b0;
+                    check_done <= 1'b0;
+                end
+
+                // Comparison logic for box:
+
+            end
             endcase
         end
     end
